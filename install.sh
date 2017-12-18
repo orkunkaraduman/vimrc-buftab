007341484621dd73b5d87ec6866079afe5506ed3
Orkun karaduman <orkunkaraduman@gmail.com>
2017-12-18 16:30:35 +0300

    fix SymLink problem in readme and install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ git clone https://github.com/orkunkaraduman/vimrc-buftab.git  ~/.vim/vimrc-bufta
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # SymLink vimrc
-ln -f ~/.vim/vimrc-buftab/vimrc ~/.vimrc
+ln -s -f ~/.vim/vimrc-buftab/vimrc ~/.vimrc
 
 # Install plugins
 vim +PluginInstall +qall
